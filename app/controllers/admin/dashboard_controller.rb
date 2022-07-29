3aed7e876f3ee857a7c0b72e292cc89f5522863f
Abigail Addae <abigail2addae@gmail.com>
2022-07-29 02:33:16 -0400

    change env var name

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
   def show
     @product_count = Product.count
     @category_count = Category.count
