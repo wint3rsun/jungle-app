0742e858d468ce757da70a1917a898f2b93c59bf
Abigail Addae <abigail2addae@gmail.com>
2022-08-03 11:00:16 -0400

    add validation for min. password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: {case_sensitive: false}
-  validates :password, presence: true
+  validates :password, presence: true, length: {minimum: 7}
   validates :password_confirmation, presence: true
 
 end
