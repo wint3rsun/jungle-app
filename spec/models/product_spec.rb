919f139e7bde669af34cb86b42fb4a858746d22f
Abigail Addae <abigail2addae@gmail.com>
2022-08-02 23:45:28 -0400

    add spec tests for product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    it 'should save successfully' do
+    before do
       @category = Category.create(name:'Games')
+    end
+
+    it 'should save successfully' do
       product = Product.new(
         name: 'Pokemon Go',
         price_cents: 100.00,
@@ -12,5 +15,46 @@ RSpec.describe Product, type: :model do
       )
       expect(product).to be_valid
     end
+
+    it 'should validate that a name is present' do
+      product = Product.new(
+        name: nil,
+        price_cents: 100.00,
+        quantity: 50,
+        category_id: @category.id
+      )
+      expect(product).to be_invalid
+    end
+
+    it 'should validate that a price is present' do
+      product = Product.new(
+        name: 'Pokemon Go',
+        price_cents: nil,
+        quantity: 50,
+        category_id: @category.id
+      )
+      expect(product).to be_invalid
+    end
+
+    it 'should validate that a quantity is present' do
+      product = Product.new(
+        name: 'Pokemon Go',
+        price_cents: 100.00,
+        quantity: nil,
+        category_id: @category.id
+      )
+      expect(product).to be_invalid
+    end
+
+    it 'should validate that a category is present' do
+      product = Product.new(
+        name: 'Pokemon Go',
+        price_cents: 100.00,
+        quantity: 50,
+        category_id: nil
+      )
+      expect(product).to be_invalid
+    end
+
   end
 end
